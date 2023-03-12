64078cf92eeeee6332fc50404bd5a7dda4f01ad5
Sebasrojas01 <juan.rojas05@correo.usa.edu.co>
2023-03-12 16:02:02 +0000

    Cambios programas

--- lambda1/apps.py
+++ lambda1/apps.py
@@ -4,29 +4,29 @@ import datetime
 
 
 def get_url():
-    return 'https://casas.mitula.com.co/searchRE/nivel3-Chapinero/nivel2-Bogot%C3%A1/nivel1-Cundinamarca/tipo-Casa/q-Bogot%C3%A1-Chapinero' 
+    return 'https://casas.mitula.com.co/searchRE/nivel3-Chapinero/nivel2-Bogo'\
+            + 't%C3%A1/nivel1-Cundinamarca/tipo-Casa/q-Bogot%C3%A1-Chapinero'
 
 
 def download(url):
     response = requests.get(url)
-    
     return response
+
 
 def upload(file_name, response):
     # Guardar la página en el bucket de S3
     s3 = boto3.resource('s3')
-    s3.Bucket('landing-casas-2302').put_object(Key=file_name, Body=response.content)
+    s3.Bucket('landing-casas-124').put_object(Key=file_name,
+                                              Body=response.content)
+
 
 def get_file_name():
-    
     now = datetime.datetime.now()
     file_name = now.strftime("%Y-%m-%d") + ".html"
-    
     return file_name
-    
+
+
 def f():
-    
     file_name = get_file_name()
-    
     response = download(get_url())
     upload(file_name, response)
--- lambda1/test_apps.py
+++ lambda1/test_apps.py
@@ -3,19 +3,20 @@ from apps import download, get_file_name, get_url
 
 
 def test_apps_download(mocker):
-    
-    mocker.patch("requests.get", return_value = True)
-    html = download('https://casas.mitula.com.co/searchRE/nivel3-Chapinero/nivel2-Bogot%C3%A1/nivel1-Cundinamarca/tipo-Casa/q-Bogot%C3%A1-Chapinero')
-    
-    assert html 
+    mocker.patch("requests.get", return_value=True)
+    html = download('https://casas.mitula.com.co/searchRE/nivel3-Chapinero'
+                    + '/nivel2-Bogot%C3%A1/nivel1-Cundinamarca/tipo-Casa'
+                    + '/q-Bogot%C3%A1-Chapinero')
+    assert html
 
-def test_apps_get_file_name(): 
+
+def test_apps_get_file_name():
     now = datetime.datetime.now()
     file_name = now.strftime("%Y-%m-%d") + ".html"
-    
-    assert  get_file_name() == file_name
+    assert get_file_name() == file_name
 
 
 def test_apps_get_url():
-    assert  get_url() == 'https://casas.mitula.com.co/searchRE/nivel3-Chapinero/nivel2-Bogot%C3%A1/nivel1-Cundinamarca/tipo-Casa/q-Bogot%C3%A1-Chapinero'
-    
+    assert get_url() == 'https://casas.mitula.com.co/searchRE/'\
+                        + 'nivel3-Chapinero/nivel2-Bogot%C3%A1/nivel1'\
+                        + '-Cundinamarca/tipo-Casa/q-Bogot%C3%A1-Chapinero'
